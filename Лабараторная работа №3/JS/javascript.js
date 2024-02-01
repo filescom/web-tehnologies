d906509450a9931ca7bcb0edb04161210faac5d0
Dmitriy <dim81295@mail.ru>
2024-02-01 12:54:20 +0300

    Лабораторные работы

---
+++ Лабараторная работа №3/JS/javascript.js
@@ -0,0 +1,170 @@
+
+
+/*Первое задание*/
+
+let button_one = document.getElementById('button');
+button_one.addEventListener("click", function(){
+	button_one.innerHTML = "<b>Полужирный шрифт</b><br><i>Курсив</i><br><center>Строка расположена по центру</center>";
+})
+
+/*Второе задание*/
+
+let img2 = document.querySelector('#img_work');
+
+function rotate() {
+	img2.style.transform = "rotate(33deg)";
+}
+function norotate() {
+	img2.style.transform = "rotate(0deg)";
+}
+function increase() {
+	img2.style.width = "245px";
+	img2.style.height = "275px";
+}
+
+img2.addEventListener('mouseover', rotate);
+img2.addEventListener('mouseout', norotate);
+img2.addEventListener('click', increase);
+
+let time = document.querySelector('.work_three');
+
+/*Третье задание*/
+
+
+function timeInput() {
+	DateObj = new Date();
+let hours = DateObj.getHours();
+let min = DateObj.getMinutes();
+let sec = DateObj.getSeconds();
+if (min < 10) {
+	min = "0" + min;
+}
+if (sec < 10) {
+	sec = "0" + sec;
+}
+if  (hours < 10) {
+	hours = "0" + hours;
+}
+let timeMoscow = (`Московское время: ${hours} : ${min} : ${sec}`);
+
+
+document.getElementById("textTime").value = timeMoscow;
+setTimeout('timeInput()', 1000);
+}
+
+function days() {
+	Days = new Array("воскресенье", "понедельник", "вторник",
+	 "среда", "четверг", "пятница", "суббота");
+daysObj = new Date();
+day = daysObj.getDay();
+numberDay = daysObj.getDate();
+monthList = new Array("Января", "Февраля", "Марта", "Апреля", "Мая",
+"Июня", "Июля", "Августа", "Сентября", "Ноября", "Декабря",);
+let month = daysObj.getMonth();
+	month -= 1;
+let year = daysObj.getYear();
+if (year < 2000)
+	{
+	years = 1900 + year;
+	}
+else
+	{
+	years = year;
+	}
+let dateOut = (`${numberDay} ${monthList[month]} ${years} года, ${Days[day]}`);
+document.getElementById("textDays").value = dateOut;
+}
+
+days();
+
+/*Четвертое задание*/
+
+function inputText(){
+	let textInput = document.getElementById('textInput');
+	 textInput.value = textInput.value.replace(/%/g, '@');
+}
+document.getElementById('buttonText').addEventListener('click', inputText);
+
+/*Пятое задание*/
+
+/*Первый прямоугольник*/
+let rectangleOne = document.getElementById("rectangleOne");
+let ctxOne = rectangleOne.getContext("2d");
+ctxOne.fillStyle = "#00FF00";
+ctxOne.fillRect(0,0,200,100);
+
+
+/*Второй прямоугольник*/
+let rectangleTwo = document.getElementById("rectangleTwo");
+let ctxTwo = rectangleTwo.getContext("2d");
+ctxTwo.fillStyle = "#FFFF00";
+ctxTwo.fillRect(0,0,200,100);
+
+
+/*Ромб*/
+
+let canvas = document.getElementById('myCanvas');
+let ctx = canvas.getContext('2d');
+
+ctx.strokeStyle = '#ff0000'; 
+ctx.fillStyle = '#ffff00'; 
+
+ctx.beginPath();
+ctx.moveTo(100, 10); 
+ctx.lineTo(190, 100); 
+ctx.lineTo(100, 190); 
+ctx.lineTo(10, 100); 
+ctx.closePath();
+ctx.fill();
+ctx.stroke();
+
+/*Приветствие canvas*/
+
+
+let canvasText = document.getElementById('canvasText');
+let context = canvasText.getContext('2d');
+context.fillStyle = 'yellow';
+context.strokeStyle = 'purple';
+context.font = '30px Arial';
+let text = 'Привет, мир!';
+let x = 50;
+let y = 100;
+context.fillText(text, x, y);
+context.strokeText(text, x, y);
+
+/*Шестое задание*/
+
+let calcOneInput = document.getElementById('calcOne');
+let calcTwoInput = document.getElementById('calcTwo');
+let calcSum = document.getElementById('calcSum');
+let calcSub = document.getElementById('calcSub');
+let calcMult = document.getElementById('calcMult');
+let calcDiv = document.getElementById('calcDiv');
+let result;
+
+calcSum.addEventListener('click', function(){
+	let calcOne = parseFloat(calcOneInput.value);
+	let calcTwo = parseFloat(calcTwoInput.value);
+	result = calcOne + calcTwo;
+	alert(result);
+})
+calcSub.addEventListener('click', function(){
+	let calcOne = parseFloat(calcOneInput.value);
+	let calcTwo = parseFloat(calcTwoInput.value);
+	result = calcOne - calcTwo;
+	alert(result);
+})
+calcMult.addEventListener('click', function(){
+	let calcOne = parseFloat(calcOneInput.value);
+	let calcTwo = parseFloat(calcTwoInput.value);
+	result = calcOne * calcTwo;
+	alert(result);
+})
+calcDiv.addEventListener('click', function(){
+	let calcOne = parseFloat(calcOneInput.value);
+	let calcTwo = parseFloat(calcTwoInput.value);
+	result = calcOne / calcTwo;
+	alert(result);
+})
+let operations = document.getElementById('operations');
+
