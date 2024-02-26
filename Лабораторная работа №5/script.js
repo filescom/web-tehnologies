6670bf9cc6b89c1290d41baf7d59c26ec1b4d072
Dmitriy <dim81295@mail.ru>
2024-02-26 13:24:32 +0300

    сделано 3 задание 5 лабораторной работы

--- Лабораторная работа №5/script.js
+++ Лабораторная работа №5/script.js
@@ -10,4 +10,30 @@ $(document).ready(function(){
     $( function() {
         $( "#schools" ).accordion();
       } );
+
+    //Полоса загрузки
+
+    let progressBar = $("#progress");
+    let dialogMessage = $("#dialog-message").dialog({
+      autoOpen: false,
+      modal: true,
+      buttons: {
+        "OK": function () {
+          $(this).dialog("close");
+        }
+      }
+    });
+
+    let totalTime = 60;
+    let currentTime = 0;
+
+    let interval = setInterval(function () {
+      currentTime++;
+      let progress = (currentTime / totalTime) * 100;
+      progressBar.css("width", progress + "%");
+      if (currentTime >= totalTime) {
+        clearInterval(interval);
+        dialogMessage.dialog("open");
+      }
+    }, 1000);
 });
